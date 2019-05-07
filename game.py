169905cc25013f066e87299b4fd143cba6d67206
Myles Evans <mevans2020@ravenscroft.org>
2019-05-07 14:30:08 -0400

    main functions

--- game.py
+++ game.py
@@ -2,15 +2,34 @@ import pygame, sys, time
 from pygame.locals import *
 
 from ship import ship
+ufo=ship()
+FPS = 90
 
-
+fpsClock = pygame.time.Clock()
 
 
 DISPLAYSURF = pygame.display.set_mode((1000,500), 0, 32)
 pygame.display.set_caption("Scrambler Remake")
-
+x=0
 while True:
+    if x==0:
+        DISPLAYSURF.blit(ufo.image,ufo.rect)
     for event in pygame.event.get():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type==KEYDOWN:
+                if event.key==K_UP:
+                    ship.up()
+                    DISPLAYSURF.blit(ufo.image,ufo.rect)
+                if event.key==K_DOWN:
+                    ship.down()
+                    DISPLAYSURF.blit(ufo.image,ufo.rect)
+                if event.key==K_RIGHT:
+                    ship.right()
+                    DISPLAYSURF.blit(ufo.image,ufo.rect)
+                if event.key==K_LEFT:
+                    ship.left()
+                    DISPLAYSURF.blit(ufo.image,ufo.rect)
+    pygame.display.update()
+    fpsClock.tick(FPS)
--- ship.py
+++ ship.py
@@ -1,17 +1,26 @@
 import pygame, sys, time, random
 from pygame.locals import *
 
-FROG = pygame.image.load('resources/frog.png')
+SHIP = pygame.image.load('resources/UFO.png')
 class ship(pygame.sprite.Sprite):
     def __init__(self):
-        #When frog calss is ran
         super().__init__()
-        self.image = FROG
+        self.image = SHIP
         #Image name
-        self.imgage = pygame.transform.scale(FROG, (25,25))
-        #Size of image
-        self.points = 0
+        self.imgage = pygame.transform.scale(SHIP, (25,25))
+        self.ground=ground
         self.x = 200
         self.y = 280
-        #Spawn
         self.rect = pygame.Rect(self.x, self.y, 25, 25)
+
+    def up(self):
+        self.rect.y = self.rect.y - 25
+
+    def down(self):
+        self.rect.y = self.rect.y + 25
+
+    def left(self):
+        self.rect.x = self.rect.x - 25
+
+    def right(self):
+        self.rect.x = self.rect.x + 25
