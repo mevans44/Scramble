6007e9290fbd5f3f62885ec994c16d0a3d1a1fac
Myles Evans <mevans2020@ravenscroft.org>
2019-05-07 13:47:21 -0400

    main screen
    Got the main screen to appear

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
-import
+import pygame, sys, time
+from pygame.locals import *
+
+from ship import ship
+
+
+
+
+DISPLAYSURF = pygame.display.set_mode((1000,500), 0, 32)
+pygame.display.set_caption("Scrambler Remake")
+
+while True:
+    for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
--- ship.py
+++ ship.py
@@ -1,1 +1,17 @@
-import
+import pygame, sys, time, random
+from pygame.locals import *
+
+FROG = pygame.image.load('resources/frog.png')
+class ship(pygame.sprite.Sprite):
+    def __init__(self):
+        #When frog calss is ran
+        super().__init__()
+        self.image = FROG
+        #Image name
+        self.imgage = pygame.transform.scale(FROG, (25,25))
+        #Size of image
+        self.points = 0
+        self.x = 200
+        self.y = 280
+        #Spawn
+        self.rect = pygame.Rect(self.x, self.y, 25, 25)
