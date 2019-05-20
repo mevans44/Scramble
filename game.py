bb7b307a89df92fbf9b78e5730b6f68f962e7e9b
Myles Evans <mevans2020@ravenscroft.org>
2019-05-20 10:15:06 -0400

    moving background

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ from fireball import fireball
 ufo=ship()
 fye=fire()
 bal=fireball()
-FPS = 90
+FPS = 1500000
 WHITE=(250,250,250)
 fpsClock = pygame.time.Clock()
 def update_ship():
@@ -20,12 +20,25 @@ def update_fireball():
 DISPLAYSURF = pygame.display.set_mode((1024,576), 0, 32)
 pygame.display.set_caption("Scrambler Remake")
 
-BACKGROUND = pygame.image.load('resources/space.png')
+BACKGROUND = pygame.image.load('resources/spacex.png')
 background_x = 0
-background_y = 0
+background_y = BACKGROUND.get_width()
+clock=pygame.time.Clock()
+speed=60
+def bg():
+    DISPLAYSURF.blit (BACKGROUND, (background_x,0))
+    DISPLAYSURF.blit(BACKGROUND,(background_y,0))
 
 while True:
-    DISPLAYSURF.blit(BACKGROUND,(background_x, background_y))
+    bg()
+    clock.tick(speed)
+    background_x -= 5.0
+    background_y -= 5.0
+    if background_x < BACKGROUND.get_width() * -1:
+        background_x= BACKGROUND.get_width()
+
+    if background_y < BACKGROUND.get_width() * -1:
+        background_y= BACKGROUND.get_width()
 
     DISPLAYSURF.blit(ufo.image,ufo.rect)
     DISPLAYSURF.blit(fye.image,fye.rect)
@@ -36,7 +49,6 @@ while True:
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
-                update_ship()
                 update_fire()
                 update_fireball()
             if event.type==KEYDOWN:
@@ -55,6 +67,7 @@ while True:
                 if event.key==K_LEFT:
                     ufo.left()
                     DISPLAYSURF.blit(ufo.image,ufo.rect)
+            update_ship()
 
     pygame.display.flip()
     fpsClock.tick(FPS)
